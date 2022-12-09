ca620a3ce44932c8ba59f8d648f058a8875ab816
gunmala suri <gunmala@gunmalas-MacBook-Pro.local>
2022-12-09 20:13:00 +0530

    2nd commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,34 +4,44 @@ import (
 	"flag"
 	"os"
 	"time"
+
+	"github.com/gofiber/fiber/middleware/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 )
 
-
 var (
-	addr = flag.String("addr,": "", os.Getenv("PORT"),"")
+	addr = flag.String("addr,", "", os.Getenv("PORT"), " ")
 	cert = flag.String("cert", "", "")
-	key = flag.String("key", "", "")
-
+	key  = flag.String("key", "", "")
 )
 
 func Run() error {
 	flag.Parse()
 
-	if *addr == ":"{
+	if *addr == ":" {
 		*addr = ":8080"
 	}
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
 
 	app.Get("/", handlers.Welcome)
 	app.Get("/room/create", handlers.RoomCreate)
 	// to visit that unique room, we use uuid
 	app.Get("/room/:uuid", handlers.Room)
-	app.Get("/room/:uuid/websocket", )
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebsocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWebsocket))
 	app.Get("/room/uuid/viewer/websocket", websocket.New(handlers.RoomViewerWebsocket))
 	app.Get("/stream/:ssuid", handlers.Stream)
-	app.Get("/stream/:ssuid/websocket",)
-	app.Get("/stream/ssuid/chat/websocket",)
+	app.Get("/stream/:ssuid/websocket")
+	app.Get("/stream/ssuid/chat/websocket")
 	app.Get("/stream/:ssuid/viewer/websocket")
-
 }

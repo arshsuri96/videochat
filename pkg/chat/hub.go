47a62096eea5f39cfa81967805586005903534dc
gunmala suri <gunmala@gunmalas-MacBook-Pro.local>
2022-12-14 11:19:39 +0530

    hub file updated

--- pkg/chat/hub.go
+++ pkg/chat/hub.go
@@ -10,3 +10,36 @@ type Hub struct {
 	// these are channels which will tell if the client is
 	// on the page or not, registers - when somebody registers on the room.
 }
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.Send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}

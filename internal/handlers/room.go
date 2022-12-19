da9f59d55757ff14bc15507373b9ba0026d9e204
gunmala suri <gunmala@gunmalas-MacBook-Pro.local>
2022-12-19 11:37:56 +0530

    added handlers>room.go

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -65,6 +65,20 @@ func RoomViewerWebsocket(c *websocket.Conn) {
 }
 
 func roomViewerConn(c *websocket.Conn, p *w.Peers) {
+	ticker := time.Newticker(1 * time.Second)
+	defer ticker.Stop()
+	defer c.Close()
+
+	for {
+		select {
+		case <-ticker.C:
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connections))))
+		}
+	}
 
 }
 
